1f0ef50509567fed64887b81f2c741656e0bb633
mujtabaalam <mujtaba.alam.khan@gmail.com>
2016-08-31 11:15:25 +0300

    first commit

---
+++ Classifieds/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Classifieds
+//
+//  Created by Mujtaba Alam on 8/31/16.
+//  Copyright © 2016 Mujtaba Alam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Classifieds/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Classifieds
+//
+//  Created by Mujtaba Alam on 8/31/16.
+//  Copyright © 2016 Mujtaba Alam. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
